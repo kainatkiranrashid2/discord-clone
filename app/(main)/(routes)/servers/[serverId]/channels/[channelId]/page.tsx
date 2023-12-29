f9682b7b9e79b617b0d6959850e11e05e3ba4c68
Kainat Kiran Rashid <kainat.kiran@cache-cloud.com>
2023-12-29 17:22:41 +0500

    message attachment

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -11,34 +12,47 @@ interface ChannelIdPageProps {
   };
 }
 const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
-    const profile = await currentProfile();
+  const profile = await currentProfile();
 
-    if (!profile) {
-      return redirectToSignIn();
-    }
-  
-    const channel = await db.channel.findUnique({
-      where: {
-        id: params.channelId,
-      },
-    });
-  
-    const member = await db.member.findFirst({
-      where: {
-        serverId: params.serverId,
-        profileId: profile.id,
-      }
-    });
-  
-    if (!channel || !member) {
-      redirect("/");
-    }
-  return  <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-  <ChatHeader
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
-      /></div>;
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
